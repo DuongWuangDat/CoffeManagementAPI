72340494cd90775200f4021b5b8394cef39319ed
Tran Thanh Dat <neban0444@gmail.com>
2025-06-08 23:30:35 +0700

    apply state patern

---
+++ CoffeeManagementAPI/State/TableState/NotBookedState.cs
@@ -0,0 +1,21 @@
+﻿namespace CoffeeManagementAPI.State.TableState
+{
+    public class NotBookedState : ITableState
+    {
+        private TableContext _context;
+
+        public void SetContext(TableContext context)
+        {
+            _context = context;
+        }
+
+        public string GetStatus() => "Not booked";
+        public bool CanDelete() => true;
+
+        public async Task<bool> HandleAsync()
+        {
+            return await Task.FromResult(true);
+        }
+    }
+
+}
---
+++ CoffeeManagementAPI/State/TableState/TableContext.cs
@@ -0,0 +1,46 @@
+﻿using CoffeeManagementAPI.Factory;
+
+namespace CoffeeManagementAPI.State.TableState
+{
+    public class TableContext
+    {
+        private ITableState _state;
+        public Model.Table Table { get; }
+
+        public TableContext(Model.Table table, ITableState initialState = null)
+        {
+            Table = table;
+            if (initialState != null)
+                ChangeState(initialState);
+            else
+                SetStateFromStatus(table.Status);
+        }
+
+        public void ChangeState(ITableState state)
+        {
+            _state = state;
+            _state.SetContext(this);
+            Table.Status = state.GetStatus(); // cập nhật trạng thái thực tế trong DB
+        }
+
+        public async Task<bool> HandleAsync()
+        {
+            return await _state.HandleAsync();
+        }
+
+        public string GetStatus()
+        {
+            return _state.GetStatus();
+        }
+        public bool CanDelete()
+        {
+            return _state.CanDelete();
+        }
+
+        private void SetStateFromStatus(string status)
+        {
+            ChangeState(TableStateFactory.Create(status));
+        }
+    }
+
+}
